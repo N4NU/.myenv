600b87ef7ef73895910acdca455907e6e3634caf
Ikumi Shimizu <193s@users.noreply.github.com>
2015-01-01 16:46:20 +0900

    fix

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,14 +2,14 @@
 
 readonly DIR=`dirname $0`
 
-read -p "install NeoBundle? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-fi
 
 cp -i $DIR/.vimrc ~
 cp -i $DIR/.vimrc.profile ~
 cp -ir $DIR/.vim/ ~
 
+read -p "install NeoBundle? " -n 1 -r
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+fi
